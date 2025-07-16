43a5b3b6a3a94c686729ee9d59cd9b9a48c64653
Dmytro <origami668@gmail.com>
2025-07-16 14:51:53 +0200

    checkpoint before OO

--- Map.cpp
+++ Map.cpp
@@ -1,56 +1,215 @@
 #include "Map.h"
 #include <iostream>
 
-GameMap read_from_file(std::string file_path) {
+// FIXME: this code is FUCKING ABHORRENT.
+
+
+void load_map_from_file(GameMap& map, std::string file_path) {
     std::ifstream file(file_path);
 
     if (!file) {
         std::cerr << "Failed to open file: " << file_path << std::endl;
-        return {{{{0, 0}, {0, 0}}}};
+        return;
     }
 
-    GameMap map;
+    map.walls.clear();
 
     std::pair<Vector2, Vector2> wall;
     int currentNumber = 0;
 
     bool secondRun = false;
     bool negated = false;
+    bool comment = false;
 
     char ch;
     while (file.get(ch)) {
-        if (ch == '-') {
+        if (comment) {
+            if (ch == '/') {
+                comment = false;
+            }
+        } else if (ch == '/')
+            comment = true;
+        else if (ch == '-') {
             negated = true;
         } else if (ch >= '0' && ch <= '9') {
             currentNumber = currentNumber * 10 + ch - '0';
-            if (negated) {
-                currentNumber = -currentNumber;
-                negated = false;
-            }
         } else if (ch == ',') {
             if (!secondRun)
-                wall.first.x = currentNumber;
+                wall.first.x = negated ? -currentNumber : currentNumber;
             else
-                wall.second.x = currentNumber;
+                wall.second.x = negated ? -currentNumber : currentNumber;
             currentNumber = 0;
+            negated = false;
         } else if (ch == ';') {
-            wall.first.y = currentNumber;
+            wall.first.y = negated ? -currentNumber : currentNumber;
             currentNumber = 0;
+            negated = false;
             secondRun = true;
         } else {
-            wall.second.y = currentNumber;
+            wall.second.y = negated ? -currentNumber : currentNumber;
             currentNumber = 0;
             secondRun = false;
+            negated = false;
             map.walls.push_back(wall);
         }
     }
 
     if (secondRun) {
-        wall.second.y = currentNumber;
+        wall.second.y = negated ? -currentNumber : currentNumber;
         map.walls.push_back(wall);
     }
 
     file.close();
+}
 
-    return map;
+void load_trigger_areas(GameMap& map, std::string file_path) {
+    std::ifstream file(file_path);
+
+    if (!file) {
+        std::cerr << "Failed to open file: " << file_path << std::endl;
+        return;
+    }
+
+    map.trigger_areas.clear();
+
+    TriggerArea area;
+
+    int currentNumber = 0;
+
+    bool secondRun = false;
+    bool negated = false;
+    bool comment = false;
+    bool readingCamFocus = false;
+
+    char ch;
+    while (file.get(ch)) {
+        if (comment) {
+            if (ch != '/') {
+                continue;
+            }
+            comment = false;
+            continue;
+        }
+
+        if (readingCamFocus) {
+            if (ch != '!') {
+                area.action_opt += ch;
+                continue;
+            }
+            readingCamFocus = false;
+            continue;
+        }
+
+        if (ch == '/') {
+            comment = true;
+        } else if (ch == '-') {
+            negated = true;
+        } else if (ch >= '0' && ch <= '9') {
+            currentNumber = currentNumber * 10 + ch - '0';
+        } else if (ch == ',') {
+            if (!secondRun)
+                area.area.first.x = negated ? -currentNumber : currentNumber;
+            else
+                area.area.second.x = negated ? -currentNumber : currentNumber;
+            currentNumber = 0;
+            negated = false;
+        } else if (ch == ';') {
+            area.area.first.y = negated ? -currentNumber : currentNumber;
+            currentNumber = 0;
+            negated = false;
+            secondRun = true;
+        } else if (ch == '@') {
+            area.action = Action::FREEZE_PLAYER;
+        } else if (ch == '!') {
+            area.action = Action::FOCUS_CAMERA;
+            readingCamFocus = true;
+        } else {
+            area.area.second.y = negated ? -currentNumber : currentNumber;
+            currentNumber = 0;
+            negated = false;
+            secondRun = false;
+            map.trigger_areas.push_back(area);
+            area.action_opt.clear();
+        }
+    }
+
+    if (secondRun) {
+        area.area.second.y = negated ? -currentNumber : currentNumber;
+        map.trigger_areas.push_back(area);
+    }
+
+    file.close();
+}
+
+
+void load_entities_from_file(GameMap &map, std::string file_path) {
+    std::ifstream file(file_path);
+
+    if (!file) {
+        std::cerr << "Failed to open file: " << file_path << std::endl;
+        return;
+    }
+
+    map.enemies.clear();
+
+    Entity entity_temp;
+
+    bool firstRun = true;
+    bool negated = false;
+    bool comment = false;
+
+    bool readingName = false;
+    std::string name;
+
+    int currentNumber = 0;
+
+
+    char ch;
+    while (file.get(ch)) {
+        if (comment) {
+            if (ch != '/') {
+                continue;
+            }
+            comment = false;
+            continue;
+        }
+
+        if (readingName) {
+           if (ch != '@') {
+               if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
+                   name += ch;
+               continue;
+           }
+            readingName = false;
+        }
+
+        else if (ch == '-') {
+            negated = true;
+        } else if (ch >= '0' && ch <= '9') {
+            currentNumber = currentNumber * 10 + ch - '0';
+        } else if (ch == ',') {
+            entity_temp.position.x = negated ? -currentNumber*50 : currentNumber*50;
+            currentNumber = 0;
+            negated = false;
+        } else if (ch == ';') {
+            entity_temp.position.y = negated ? -currentNumber*50 : currentNumber*50;
+            currentNumber = 0;
+            negated = false;
+        } else if (ch == '/') comment = true;
+        else if (ch == '@') {
+            readingName = true;
+        } else {
+            entity_temp.size = {50, 50};
+            entity_temp.health = currentNumber;
+            entity_temp.reload_clock = 0.0f;
+            entity_temp.reload_time = 0.50f;
+            entity_temp.name = name;
+            name.clear();
+            if (firstRun) map.player = entity_temp;
+            else map.enemies.push_back(entity_temp);
+            entity_temp = {};
+            currentNumber = 0;
+            firstRun = false;
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,86 +1,9 @@
-#include <format>
-#include <vector>
+#include "Map.h"
+#include "auxillary_functions.h"
 #include "raylib.h"
 #include "raymath.h"
-#include "Map.h"
-
-typedef struct {
-    Vector2 position;
-    Vector2 size;
-    int health;
-    float vel_x;
-    float vel_y;
-} Entity;
-
-
-void move_player(Entity& player) {
-    float delta = GetFrameTime();
-    float speed = 300.0f;
-
-    if (IsKeyDown(KEY_W)) {
-        player.vel_y -= speed*delta;
-    }
-    if (IsKeyDown(KEY_A)) {
-        player.vel_x -= speed*delta;
-    }
-    if (IsKeyDown(KEY_S)) {
-        player.vel_y += speed*delta;
-    }
-    if (IsKeyDown(KEY_D)) {
-        player.vel_x += speed*delta;
-    }
-}
-
-void collision_x(Entity& player, std::pair<float, float> wall_x) {
-    float wall_left = wall_x.first * 50;
-    float wall_right = wall_left + wall_x.second * 50;
-    float player_left = player.position.x + player.vel_x;
-    float player_right = player_left + player.size.x;
-
-    if (player_right > wall_left && player_left < wall_right) {
-        if (player.vel_x > 0) {
-            player.vel_x = wall_left - (static_cast<double>(player.position.x) + static_cast<double>(player.size.x)); // I legitimately have zero idea why, but this cast allows
-                                                                                                                      // the player to collide with walls on the side normally
-                                                                                                                      // instead of SOMETIMES hovering one pixel away from them...
-        }
-        else if (player.vel_x < 0) {
-            player.vel_x = wall_right - player.position.x;
-        }
-    }
-}
-
-void collision_y(Entity& player, std::pair<float, float> wall_y) {
-    float wall_top = wall_y.first * 50;
-    float wall_bottom = wall_top + wall_y.second * 50;
-    float player_top = player.position.y + player.vel_y;
-    float player_bottom = player_top + player.size.y;
-
-    if (player_bottom > wall_top && player_top < wall_bottom) {
-        if (player.vel_y > 0) {
-            player.vel_y = wall_top - (static_cast<double>(player.position.y) + static_cast<double>(player.size.y));  // Same as above. (My best guess is FP precision loss?)
-        }
-        else if (player.vel_y < 0) {
-            player.vel_y = wall_bottom - player.position.y;
-        }
-    }
-}
-
-void player_tick(Entity& player, GameMap& map) {
-    for (const auto& wall : map.walls) {
-        if (CheckCollisionRecs({wall.first.x*50, wall.first.y*50, wall.second.x*50, wall.second.y*50},
-                               {player.position.x+player.vel_x, player.position.y, player.size.x, player.size.y})) {
-            collision_x(player, {wall.first.x, wall.second.x});
-        }
-        else if (CheckCollisionRecs({wall.first.x*50, wall.first.y*50, wall.second.x*50, wall.second.y*50},
-                               {player.position.x, player.position.y+player.vel_y, player.size.x, player.size.y})) {
-            collision_y(player, {wall.first.y, wall.second.y});
-        }
-    }
-    player.position += {player.vel_x, player.vel_y};
-    player.vel_x = 0;
-    player.vel_y = 0;
-}
-
+#include <format>
+#include <vector>
 
 typedef struct {
     Vector2 position;
@@ -88,41 +11,201 @@ typedef struct {
     Vector2 end_pos;
     float speed;
     Vector2 direction = {0, 0};
+    bool is_friendly;
 } Bullet;
 
-void shoot_bullet(std::vector<Bullet>& bullets_arr, Vector2 origin_pos, Camera2D& camera) {
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        bullets_arr.push_back((Bullet){
-            .position = origin_pos,
-            .origin_pos = origin_pos,
-            .end_pos = GetScreenToWorld2D(GetMousePosition(), camera),
-            .speed = 500.0f,
-        });
+void move_player(Entity &player) {
+    float delta = GetFrameTime();
+    float speed = 300.0f;
+
+    if (!player.frozen_state) {
+        if (IsKeyDown(KEY_W)) {
+            player.vel_y -= speed * delta;
+        }
+        if (IsKeyDown(KEY_A)) {
+            player.vel_x -= speed * delta;
+        }
+        if (IsKeyDown(KEY_S)) {
+            player.vel_y += speed * delta;
+        }
+        if (IsKeyDown(KEY_D)) {
+            player.vel_x += speed * delta;
+        }
     }
 }
 
-void move_bullet(Bullet& bullet) {
+void collision_x(Entity &player, std::pair<float, float> wall_x) {
+    float wall_left = wall_x.first * 50;
+    float wall_right = wall_left + wall_x.second * 50;
+    float player_left = player.position.x + player.vel_x;
+    float player_right = player_left + player.size.x;
+
+    if (player_right > wall_left && player_left < wall_right) {
+        if (player.vel_x > 0) {
+            player.vel_x =
+                    wall_left -
+                    (static_cast<double>(player.position.x) +
+                     static_cast<double>(
+                         player.size.x)); // I legitimately have zero idea why, but this
+            // cast allows the player to collide with walls
+            // on the side normally instead of SOMETIMES
+            // hovering one pixel away from them...
+        } else if (player.vel_x < 0) {
+            player.vel_x = wall_right - player.position.x;
+        }
+    }
+}
+
+void collision_y(Entity &player, std::pair<float, float> wall_y) {
+    float wall_top = wall_y.first * 50;
+    float wall_bottom = wall_top + wall_y.second * 50;
+    float player_top = player.position.y + player.vel_y;
+    float player_bottom = player_top + player.size.y;
+
+    if (player_bottom > wall_top && player_top < wall_bottom) {
+        if (player.vel_y > 0) {
+            player.vel_y =
+                    wall_top -
+                    (static_cast<double>(player.position.y) +
+                     static_cast<double>(player.size.y)); // Same as above. (My best guess
+            // is FP precision loss?)
+        } else if (player.vel_y < 0) {
+            player.vel_y = wall_bottom - player.position.y;
+        }
+    }
+}
+
+void player_tick(Entity &player, GameMap &map, GameState &state) {
+    for (const auto &wall: map.walls) {
+        if (CheckCollisionRecs({
+                                   wall.first.x * 50, wall.first.y * 50,
+                                   wall.second.x * 50, wall.second.y * 50
+                               },
+                               {
+                                   player.position.x + player.vel_x, player.position.y,
+                                   player.size.x, player.size.y
+                               })) {
+            collision_x(player, {wall.first.x, wall.second.x});
+        } else if (CheckCollisionRecs({
+                                          wall.first.x * 50, wall.first.y * 50,
+                                          wall.second.x * 50, wall.second.y * 50
+                                      },
+                                      {
+                                          player.position.x,
+                                          player.position.y + player.vel_y,
+                                          player.size.x, player.size.y
+                                      })) {
+            collision_y(player, {wall.first.y, wall.second.y});
+        }
+    }
+    player.position += {player.vel_x, player.vel_y};
+    player.vel_x = 0;
+    player.vel_y = 0;
+
+    for (auto colIt = map.trigger_areas.begin(); colIt != map.trigger_areas.end();) {
+        bool proceed = true;
+        if (CheckCollisionRecs({player.position.x, player.position.y, player.size.x, player.size.y},
+                               {
+                                   colIt->area.first.x * 50, colIt->area.first.y * 50, colIt->area.second.x * 50,
+                                   colIt->area.second.y * 50
+                               })) {
+            if (colIt->action == Action::FREEZE_PLAYER) {
+                freeze_player_toggle(map.player);
+                colIt = map.trigger_areas.erase(colIt);
+                proceed = false;
+            }
+            else if (colIt->action == Action::FOCUS_CAMERA) {
+                focus_camera_on_entity(map, colIt->action_opt);
+            }
+        }
+        if (proceed) {
+            ++colIt;
+        }
+    }
+
+
+    if (player.health <= 0) {
+        state = GameState::DEAD;
+    }
+
+    player.reload_clock -= GetFrameTime();
+}
+
+void shoot_bullet(Entity &player, std::vector<Bullet> &bullets_arr,
+                  Camera2D &camera) {
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+        if (player.reload_clock <= 0) {
+            Vector2 origin_pos = player.position + player.size / 2;
+            bullets_arr.push_back((Bullet){
+                .position = origin_pos,
+                .origin_pos = origin_pos,
+                .end_pos = GetScreenToWorld2D(GetMousePosition(), camera),
+                .speed = 500.0f,
+                .is_friendly = true,
+            });
+            player.reload_clock = player.reload_time;
+        }
+    }
+}
+
+void move_bullet(Bullet &bullet) {
     float delta = GetFrameTime();
 
-    if (bullet.direction == (Vector2){0, 0}) { // Bit of a hack but when will it ever matter?
+    if (bullet.direction ==
+        (Vector2){0, 0}) {
+        // Bit of a hack but when will it ever matter?
         Vector2 dir_v = Vector2Subtract(bullet.end_pos, bullet.origin_pos);
         bullet.direction = Vector2Normalize(dir_v);
     }
 
-    bullet.position += bullet.direction*bullet.speed*delta;
+    bullet.position += bullet.direction * bullet.speed * delta;
 }
 
-void bullets_tick(std::vector<Bullet>& bullets, std::vector<Entity>& enemies) {
-    for (auto bulletIt = bullets.begin(); bulletIt != bullets.end(); ) {
+void bullets_tick(std::vector<Bullet> &bullets,
+                  std::vector<std::pair<Vector2, Vector2> > walls,
+                  std::vector<Entity> &enemies, Entity &player) {
+    for (auto bulletIt = bullets.begin(); bulletIt != bullets.end();) {
         bool hit = false;
 
         move_bullet(*bulletIt);
-        for (auto& enemy : enemies) {
-            if (CheckCollisionRecs({bulletIt->position.x, bulletIt->position.y, 24, 24}, {enemy.position.x, enemy.position.y, enemy.size.x, enemy.size.y})) {
+
+        for (auto &wall: walls) {
+            if (CheckCollisionRecs(
+                {bulletIt->position.x, bulletIt->position.y, 24, 24},
+                {
+                    wall.first.x * 50, wall.first.y * 50, wall.second.x * 50,
+                    wall.second.y * 50
+                })) {
                 hit = true;
-                enemy.health -= 10;
                 bulletIt = bullets.erase(bulletIt);
                 break;
+            }
+        }
+
+        if (bulletIt->is_friendly) {
+            for (auto &enemy: enemies) {
+                if (CheckCollisionRecs(
+                    {bulletIt->position.x, bulletIt->position.y, 24, 24},
+                    {
+                        enemy.position.x, enemy.position.y, enemy.size.x,
+                        enemy.size.y
+                    })) {
+                    hit = true;
+                    enemy.health -= 10;
+                    bulletIt = bullets.erase(bulletIt);
+                    break;
+                }
+            }
+        } else {
+            if (CheckCollisionRecs(
+                {bulletIt->position.x, bulletIt->position.y, 24, 24},
+                {
+                    player.position.x, player.position.y, player.size.x,
+                    player.size.y
+                })) {
+                player.health -= 10;
+                hit = true;
+                bulletIt = bullets.erase(bulletIt);
             }
         }
 
@@ -138,12 +221,58 @@ void bullets_tick(std::vector<Bullet>& bullets, std::vector<Entity>& enemies) {
     }
 }
 
-void enemies_tick(std::vector<Entity>& Enemies) {
-    for (auto enemyIt = Enemies.begin(); enemyIt != Enemies.end(); ) {
-        if (enemyIt->health <= 0) {
-            enemyIt = Enemies.erase(enemyIt);
+bool CheckCollisionLineWall(std::pair<Vector2, Vector2> line1,
+                            std::pair<Vector2, Vector2> wall) {
+    std::vector<std::pair<Vector2, Vector2> > wall_lines = {
+        {
+            {wall.first.x * 50, wall.first.y * 50},
+            {
+                wall.first.x * 50 * wall.second.x * 50,
+                wall.first.y * 50 * wall.second.y * 50
+            }
+        },
+        {
+            {
+                wall.first.x * 50 * wall.second.x * 50,
+                wall.first.y * 50 * wall.second.y * 50
+            },
+            {wall.first.x * 50, wall.second.y * 50}
         }
-        else {
+    };
+
+    for (auto &line: wall_lines) {
+        if (CheckCollisionLines(line.first, line.second, line1.first, line1.second,
+                                nullptr)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void enemies_tick(GameMap &map, std::vector<Bullet> &bullets) {
+    for (auto enemyIt = map.enemies.begin(); enemyIt != map.enemies.end();) {
+        if (enemyIt->health <= 0) {
+            enemyIt = map.enemies.erase(enemyIt);
+        } else {
+            bool can_shoot = true;
+            if (Vector2Distance(enemyIt->position, map.player.position) < 1000) {
+                for (auto &wall: map.walls)
+                    if (CheckCollisionLineWall({map.player.position, enemyIt->position},
+                                               wall))
+                        can_shoot = false;
+                if (can_shoot)
+                    if (enemyIt->reload_clock <= 0) {
+                        bullets.push_back((Bullet){
+                            .position = enemyIt->position,
+                            .origin_pos = enemyIt->position,
+                            .end_pos = map.player.position,
+                            .speed = 500.0f,
+                            .is_friendly = false,
+                        });
+                        enemyIt->reload_clock = enemyIt->reload_time;
+                    }
+            }
+            enemyIt->reload_clock -= GetFrameTime();
             ++enemyIt;
         }
     }
@@ -153,83 +282,96 @@ int main() {
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib C++ App");
     SetTargetFPS(60);
 
-    Entity player{
-        .position = {100, 100},
-        .size = {50, 50},
-        .health = 100
+    GameMap map;
+
+    load_map_from_file(map, "map.txt");
+    load_trigger_areas(map, "areas.txt");
+    load_entities_from_file(map, "entities.txt");
+
+    GameState state = GameState::RUNNING;
+
+    map.focused_entity = &map.player;
+
+    Camera2D camera = {0};
+    camera.target = {
+        map.focused_entity->position.x + map.focused_entity->size.x / 2,
+        map.focused_entity->position.y + map.focused_entity->size.y / 2
     };
-
-    std::vector<Entity> enemies = {
-        {
-            .position = {200, 100},
-            .size = {50, 50},
-            .health = 100,
-        },
-        {
-            .position = {100, 200},
-            .size = {50, 50},
-            .health = 100,
-        },
-    };
-
-    GameMap map = read_from_file("map.txt");
-
-    Camera2D camera = { 0 };
-    camera.target = { player.position.x + player.size.x / 2, player.position.y + player.size.y / 2 };
-    camera.offset = { SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f };
+    camera.offset = {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f};
     camera.zoom = 1.0f;
 
     std::vector<Bullet> bullets;
 
     while (!WindowShouldClose()) {
-
         // Events
-        move_player(player);
-        shoot_bullet(bullets, Vector2Add(player.position, player.size/2), camera);
+        move_player(map.player);
+        shoot_bullet(map.player, bullets, camera);
 
-        if (IsKeyDown(KEY_P)) {
-            printf("player pos: %f %f\n", player.position.x, player.position.y);
-            printf("player bottom pos y: %f\n", player.position.y+player.size.y);
+        if (IsKeyPressed(KEY_P)) {
+            freeze_player_toggle(map.player);
+            focus_camera_on_entity(map, "enemy1");
         }
 
         // Logic
 
-        player_tick(player, map);
-        bullets_tick(bullets, enemies);
-        enemies_tick(enemies);
+        if (state == GameState::RUNNING) {
+            player_tick(map.player, map, state);
+            bullets_tick(bullets, map.walls, map.enemies, map.player);
+            enemies_tick(map, bullets);
+        }
 
-        camera.target = { player.position.x + player.size.x / 2, player.position.y + player.size.y / 2 };
-
+        const float smoothness = 0.1f;
+        camera.target +=
+        {
+            ((map.focused_entity->position.x + map.focused_entity->size.x / 2) -
+             camera.target.x) *
+            smoothness,
+            ((map.focused_entity->position.y + map.focused_entity->size.y / 2) -
+             camera.target.y) *
+            smoothness
+        };
+        camera.offset = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
 
         // Rendering
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        BeginMode2D(camera);
+        if (state == GameState::RUNNING) {
+            BeginMode2D(camera);
 
-        DrawText("Hello, Raylib!", 350, 280, 20, DARKGRAY);
+            DrawText("Hello, Raylib!", 350, 280, 20, DARKGRAY);
 
-        DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, DARKGREEN);
-        DrawText(std::format("Health: {}", player.health).c_str(), player.position.x - 20, player.position.y - 20, 20, GREEN);
+            DrawRectangle(map.player.position.x, map.player.position.y,
+                          map.player.size.x, map.player.size.y, DARKGREEN);
+            DrawText(std::format("Health: {}", map.player.health).c_str(),
+                     map.player.position.x - 20, map.player.position.y - 20, 20, GREEN);
 
-        for (const auto& bullet : bullets) {
-            DrawRectangle(bullet.position.x, bullet.position.y, 12, 12, BLACK);
+            for (const auto &bullet: bullets) {
+                DrawRectangle(bullet.position.x, bullet.position.y, 12, 12, WHITE);
+            }
+
+            for (const auto &enemy: map.enemies) {
+                DrawRectangle(enemy.position.x, enemy.position.y, enemy.size.x,
+                              enemy.size.y, RED);
+                DrawText(std::format("Health: {}", enemy.health).c_str(),
+                         enemy.position.x - 20, enemy.position.y - 20, 20, RED);
+            }
+
+            for (const auto &wall: map.walls) {
+                DrawRectangle(wall.first.x * 50, wall.first.y * 50, wall.second.x * 50,
+                              wall.second.y * 50, BLUE);
+            }
+
+            EndMode2D();
         }
 
-        for (const auto& enemy : enemies) {
-            DrawRectangle(enemy.position.x, enemy.position.y, enemy.size.x, enemy.size.y, RED);
-            DrawText(std::format("Health: {}", enemy.health).c_str(), enemy.position.x - 20, enemy.position.y - 20, 20, RED);
+        else if (state == DEAD) {
+            DrawText("you losar", 350, 280, 80, DARKGRAY);
         }
-
-        for (const auto& wall : map.walls) {
-            DrawRectangle(wall.first.x*50, wall.first.y*50, wall.second.x*50, wall.second.y*50, BLUE);
-        }
-
-        EndMode2D();
-
 
         EndDrawing();
     }
